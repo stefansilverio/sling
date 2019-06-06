bfbda22fcb1890e6af042777bded569de5eec74d
stefansilverio <stefansilverio@gmail.com>
2019-06-06 23:24:37 +0000

    remove hashlib

--- myprojectenv/models/user.py
+++ myprojectenv/models/user.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
-import hashlib
 
 class User(BaseModel, Base):
     """This is the class for user
