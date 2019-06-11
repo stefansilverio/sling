7a4aa39b6d33238f6f59b055304b1aad1b8bb592
truong21 <543@holbertonschool.com>
2019-06-11 21:24:43 +0000

    used ajax query to connect to backend for landing page

--- myprojectenv/web_flask/app.py
+++ myprojectenv/web_flask/app.py
@@ -9,43 +9,40 @@ import json
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 host = '127.0.0.1'
-host = '192.168.33.10'
+host = '55.55.55.5'
 
 def page_not_found(e):
     """ 404 error page for nonexistent routes """
     return jsonify({'error': "Not found"}), 404
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def login():
     """ render login homepage """
-    error = None
+    return render_template('index.html')
+
+@app.route("/landing", methods=['POST'])
+def landing():
+    """landing page"""
+    flag = 0
     if request.method == 'POST':
         uname = request.form.get('email')
+        print(uname)
         objs = storage.query("User")
         for obj in objs:
             if uname in obj.__dict__.values():
-                """
-                REDIRECT HERE!
-                """
                 print("OBJ TYPE {}".format(obj))
+                print(obj.__dict__)
+                user = obj.__dict__
+                print(user.first_name)
+                flag = 1
+            if flag == 1:    
+                return render_template('index.html')
+    return render_template('landing.html', user_info=obj.__dict__)
 
-                my_dict = obj.to_dict()
-
-                jsoned = json.dumps(my_dict)
-
-                return redirect(url_for('.landing', jsoned=jsoned))
-
-        error = 'Invalid Username or Password. Please try again'
-    return render_template('index.html')
-
-@app.route("/landing")
-def landing():
-    obj_dict = json.loads(request.args['jsoned'])
-    """
-    re-create obj
-    """
-    return render_template('landing.html')
+@app.route('/burrow', methods=['GET'])
+def burrow():
+    return '<h1> Burrow, Here!</h1>'
 
 @app.route('/lend', methods=['GET'])
 def lend():
