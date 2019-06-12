de39f163ef722ffbb70a718dc734d37863edf3fc
stefansilverio <stefansilverio@gmail.com>
2019-06-12 00:39:18 +0000

    change name of borrower columns

--- myprojectenv/models/borrower.py
+++ myprojectenv/models/borrower.py
@@ -12,7 +12,8 @@ class Borrower(BaseModel, Base):
     h_m_r_t: highest monthly rate of interest WTA
     """
     __tablename__ = 'borrowers'
-    loan_size = Column(Integer, nullable=False)
-    loan_duration = Column(Integer, nullable=False)
-    h_m_r_t = Column(Integer, nullable=False)
+    loan_size = Column(Integer, nullable=True)
+    loan_duration = Column(Integer, nullable=True)
+    interest = Column(Integer, nullable=False)
+    email = Column(String(60), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
